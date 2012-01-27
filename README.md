8b0d52e3ef45e6566b1ca42684738296d6a810e2
David King <dking@xforty.com>
2012-01-27 11:23:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Description
+===========
+
+Includes recipes for php modules that can be installed.
+
+Requirements
+============
+
+* [Opscode's php cookbook](http://community.opscode.com/cookbooks/php/)
+
+
+Attributes
+==========
+
+None
+
+Usage
+=====
+
+Include one of the module recipes in your own cookbook recipe.
+
+For example: `include_recipe "php_module:gd"`
---
+++ recipes/dom.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: php_module
+# Recipe:: dom
+#
+# Copyright 2012, xforty technologies
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if node['php']['install_method'].downcase == "package"
+  case node['platform']
+  when "centos", "redhat", "fedora"
+    package "php53-xml" do
+      action :install
+    end
+  when "debian", "ubuntu"
+    # debian php compiled with xml/dom
+  end
+end
